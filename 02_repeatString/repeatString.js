42e263171ba983fee1b0c8866dfb3b9b9c917694
GretaRa <ggretute88@gmail.com>
2022-08-02 19:02:50 +0200

    Complete TDD exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num >= 0){
+        for (let i = 0; i <= num; i++){
+            let result = string.repeat(num);
+            return(result);
+        }
+    } else{
+        return('ERROR');
+    }
+    
 };
-
+repeatString('hey', 3)
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let charArray = Array.from(string);
+    charArray = charArray.reverse();
+    let reversedString = charArray.join("");
+    return(reversedString);
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
