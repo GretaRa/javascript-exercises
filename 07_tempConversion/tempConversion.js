a2a8cf41196c0b1b9d5d8e2adcba85c38edb8595
GretaRa <ggretute88@gmail.com>
2022-08-03 16:09:53 +0200

    Complete TDD exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+ let fullCelsius = (f - 32) * 5 / 9;
+ let celsius = Math.round(fullCelsius * 10)/10
+ return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let fullFahrenheit = c * 9/5 + 32;
+  let fahrenheit= Math.round(fullFahrenheit * 10)/10
+  return fahrenheit;
 };
 
 // Do not edit below this line
